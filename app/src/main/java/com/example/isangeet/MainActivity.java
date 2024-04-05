9358b5b8af189400a730ed3ed69906cad3e3c333
Shubham-Zone <154971588+Shubham-Zone@users.noreply.github.com>
2024-04-05 07:50:03 +0530

    Pushing files

---
+++ app/src/main/java/com/example/isangeet/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.isangeet;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listview);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+//                        Toast.makeText(MainActivity.this, "Runtime Permission given", Toast.LENGTH_SHORT).show();
+                        ArrayList<File> mySongs=fetchSongs(Environment.getExternalStorageDirectory());
+                        String[] items=new String[mySongs.size()];
+                        for (int i=0;i<mySongs.size();i++){
+                            items[i]=mySongs.get(i).getName().replace(".mp3","");
+                        }
+                        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity. this, android.R.layout.simple_list_item_1,items);
+                        listView.setAdapter(adapter);
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                                Intent intent =new Intent(MainActivity.this,PlaySong.class);
+                                String currentSong=listView.getItemAtPosition(position).toString();
+                                intent.putExtra("songList",mySongs);
+                                intent.putExtra("currentSong",currentSong);
+                                intent.putExtra("position",position);
+                                startActivity(intent);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                       permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+
+        //define ActionBar object
+        ActionBar actionBar;
+        actionBar =getSupportActionBar();
+        /* Define ColorDrawable object and parse color
+         * using parseColor method
+         * with color hash code as its parameter*/
+        ColorDrawable colorDrawable =new ColorDrawable(Color.parseColor("#ffc40c"));
+
+        //set background
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+    }
+
+    public ArrayList<File> fetchSongs(File file){
+        ArrayList arrayList=new ArrayList();
+        File[] songs=file.listFiles();
+        if(songs!=null){
+            for(File myFile: songs){
+                if(!myFile.isHidden() && myFile.isDirectory()){
+                    arrayList.addAll(fetchSongs(myFile));
+                }
+                else{
+                    //dont play google maps files(start with .mp3 and .
+                    if(myFile.getName().endsWith(".mp3") && !myFile.getName().startsWith(".")){
+                        arrayList.add(myFile);
+                    }
+                }
+            }
+        }
+
+        return arrayList;
+    }
+}
